46c5941b41ed871bdd1a8c9bf48ea8b9e728e416
Vedant Waghule <waghulevedant79@gmail.com>
2026-08-19 18:35:23 +0530

    Fix database connection pooling

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,14 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+
 from app.core.settings import settings
+
 
 engine = create_engine(
     settings.DATABASE_URL,
-    echo=True
+    echo=False,
+    pool_pre_ping=True,
+    pool_recycle=300,
 )
 
 SessionLocal = sessionmaker(
-    autocommit= False,
+    autocommit=False,
     autoflush=False,
     bind=engine
 )
